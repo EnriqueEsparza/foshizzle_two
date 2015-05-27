fbf4b2c333812dcd0f561be124dfd376e9f00772
steven francisco & enriqueesparza <student@epicodus.com>
2015-05-26 17:05:42 -0700

    Does this work? foshizzle

--- lib/foshizzle.rb
+++ lib/foshizzle.rb
@@ -1,14 +1,30 @@
+require('pry')
 class String
 
   define_method(:foshizzle) do
-    word = self.split("")
-    word.each() do |letter|
-      if letter == "s"
+    array = []
+    words = self.split()
+    words.each() do |word|
+      # word = word.gsub('s','z')
+      # if letter == "s"
+      #   letter.replace("z")
+
+      # if self[0] != nil
+      #    self[0]
+      letters = word.split("")
+      letters.each_with_index() do |letter, index|
+      if letter == "s" && index != 0
         letter.replace("z")
+      # if word != word.at(0)
+      #   word.gsub!('s','z')
+      # else
+      #   array.push(word)
 
       end
     end
-    word.join()
+      array.push(letters.join())
+    end
+    array.join(" ")
 
 
 
--- spec/foshizzle_spec.rb
+++ spec/foshizzle_spec.rb
@@ -7,6 +7,10 @@ describe("String#foshizzle") do
     expect(("busy").foshizzle()).to(eq("buzy"))
   end
 
+  it("will not change the first letter in a word") do
+    expect(("Hey steve").foshizzle()).to(eq("Hey steve"))
+  end
+
   it("will not change capital letters to z") do
     expect(("Stop").foshizzle()).to(eq("Stop"))
   end
